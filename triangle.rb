4b89738ff20782915266e74b629186bc49b3c206
Michał Dul <michaldul@outlook.com>
2015-06-07 23:51:54 +0200

    true_and_false & triangle project

--- triangle.rb
+++ triangle.rb
@@ -14,7 +14,16 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
-  # WRITE THIS CODE
+  a, b, c = [a, b, c].sort()
+  if a == b || b == c
+    if a == c
+      :equilateral
+    else
+      :isosceles
+    end
+  else
+    :scalene
+  end
 end
 
 # Error class used in part 2.  No need to change this code.
